1cf27f2381db81296c1f0132592751b370e57615
medusacar <142349593+medusacar@users.noreply.github.com>
2024-10-09 11:06:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cronograma capilar',
+    'Qual é a ordem correta do cronograma capilar?',
+    'A ordem correta do cronograma capilar é nutrição, hidratação e reconstrução.'
+)
+
+criaCartao(
+    'Nutrição ',
+    'A nutrição é responsável por?',
+    'A nutrição é a responsável pela proteção do cabelo contra as agressões diárias que causam o ressecamento, o frizz e o enfraquecimento da fibra capilar'
+)
+
+criaCartao(
+    'Hidratação',
+    'A hidratação é responsável por?',
+    'A hidratação é a estrela dos cabelos com a elasticidade em dia, macios, com balanço e, claro, livres do ressecamento.'
+)
+
+criaCartao(
+    'Reconstrução',
+    'A reconstrução é responsável por?',
+    'A reconstrução é a salvadora dos fios que tenham perdido massa e estejam quebradiços e afinados por conta de processos químicos'
+)
